6f9961cfd30535ba571dd91abb9d4bd0a7d0410d
Cix8 <francesco.natal_fn@libero.it>
2022-04-27 13:53:38 +0200

    add main-game, add displayScore to show the final-result to the user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
-alert('ciao');
+const numbersArray = [];
+const userGuessNumbers = [];
+const displayNumbers = document.getElementById('numbers');
+const displayScore = document.getElementById('final-score');
+
+let score = 0;
+
+while (numbersArray.length < 5) {
+    const singleNumber = getRndInteger(1,100);
+    if (!numbersArray.includes(singleNumber)) {
+        numbersArray.push(singleNumber);
+        displayNumbers.append(`${singleNumber} `);
+    }
+}
+
+console.log(numbersArray);
+
+setTimeout(() => {
+    displayNumbers.classList.add('d-none');
+}, 30000);
+
+const timer = setTimeout(function() {
+    for (let i = 0; i < numbersArray.length; i++) {
+        const userInput = parseInt(prompt('Inserisci, uno alla volta, i numeri che hai visto prima'));
+        console.log(userInput);
+        if (numbersArray.includes(userInput) && !userGuessNumbers.includes(userInput)) {
+            userGuessNumbers.push(userInput);
+        }
+    }
+
+    console.log(userGuessNumbers);
+    score = userGuessNumbers.length;
+    let result = '';
+    if (score > 0) {
+        result = `Hai indovinato ${score} numeri: ${userGuessNumbers}`
+    } else {
+        result = `Hai indovinato ${score} numeri`
+    }
+
+    displayScore.innerHTML = result;
+    
+}, 30400)
+
+//              FUNCTIONS
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
